938edb6c81a89fdd3ab878e5f9252fb8987db72b
Sabiwc <sabinatursunbaevaa@gmail.com>
2024-06-28 00:13:04 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector ('#menu');
+const menu_list = document.querySelector ('#menu_list')
+
+
+
---
+++ js/newFile.js
@@ -0,0 +1,3 @@
+menu.addEventListener('click,'(), {
+    if(_menu_list) { }, : .classlist
+});
